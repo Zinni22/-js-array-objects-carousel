dd3d43a8bf27f5a3ae086871e1fdf733aba38026
Zineb Atlassi <atlassizineb@gmail.com>
2023-01-19 11:17:39 +0100

    Esercizio completato

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+/*
+1. creo array con i dati delle immagini
+2. seleziono ciò che mi serve dall'html e creo le singole slide che inserisco
+3. do alla prima slide la visibilià
+4. seleziono le frecce e faccio in modo che cliccando su di esse la visibilità passi da un all'altra
+*/
+
 const images = [
     {
         image: 'img/01.webp',
@@ -21,3 +28,85 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+
+const slides = document.querySelector('.slides');
+
+images.forEach((index) => {
+
+    const singleSlide = document.createElement('div');
+    singleSlide.classList.add('single-slide')
+    singleSlide.innerHTML += `
+                            <img src="${index.image}" alt="img1">
+
+                            <div class="text">
+                                <h3>${index.title}</h3>
+                                <p>${index.text}</p>
+                            </div>
+    `
+    slides.append(singleSlide);
+
+
+});
+
+// seleziono le mie classi create
+const allSlides = document.querySelectorAll('.single-slide');
+const allThumbnails = document.querySelectorAll('.single-thumbnail');
+
+let currentSlide = 0;
+
+allSlides[currentSlide].classList.add('block');
+allThumbnails[currentSlide].classList.add('active');
+
+
+// seleziono le thumbnails
+
+
+// seleziono le frecce
+const prevArrow = document.querySelector('.before');
+const nextArrow = document.querySelector('.after');
+
+
+
+
+prevArrow.addEventListener('click',
+
+    function () {
+
+        allSlides[currentSlide].classList.remove('block');
+        allThumbnails[currentSlide].classList.remove('active');
+
+
+        if(currentSlide > 0){
+            currentSlide--;
+        }
+        else{
+            currentSlide = allSlides.length - 1;
+        }
+
+        allSlides[currentSlide].classList.add('block');
+        allThumbnails[currentSlide].classList.add('active');
+
+    }
+);
+
+nextArrow.addEventListener('click',
+
+    function () {
+
+        allSlides[currentSlide].classList.remove('block');
+        allThumbnails[currentSlide].classList.remove('active');
+
+
+        if(currentSlide < allSlides.length - 1){
+            currentSlide++;
+        }
+        else{
+            currentSlide = 0;
+        }
+
+        allSlides[currentSlide].classList.add('block');
+        allThumbnails[currentSlide].classList.add('active');
+
+    }
+);
